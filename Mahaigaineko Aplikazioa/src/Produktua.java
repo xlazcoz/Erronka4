98486e081ad53194b2c841874f4314fb77e66340
Xabier Lazcoz <ikxlazcoz25@lhusurbil.eus>
2026-02-25 08:05:23 +0100

    produktua

--- Mahaigaineko Aplikazioa/src/Produktua.java
+++ Mahaigaineko Aplikazioa/src/Produktua.java
@@ -1,27 +1,46 @@
 public class Produktua {
     private int id;
-    private String izena;
     private String deskribapena;
+    private String izena;
     private double prezioa;
     private int stocka;
+    private String sorkuntzaData;
     private int kategoriaId;
-    private String irudiaUrl;
 
     public Produktua() {}
 
-    public Produktua(String izena, String deskribapena, double prezioa, int stocka, int kategoriaId, String irudiaUrl) {
+    public Produktua(String izena, String deskribapena, double prezioa, int stocka, String sorkuntzaData, int kategoriaId) {
         this.izena = izena;
         this.deskribapena = deskribapena;
         this.prezioa = prezioa;
         this.stocka = stocka;
+        this.sorkuntzaData = sorkuntzaData;
         this.kategoriaId = kategoriaId;
-        this.irudiaUrl = irudiaUrl;
     }
 
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
     public String getIzena() { return izena; }
+    public void setIzena(String izena) { this.izena = izena; }
+
     public String getDeskribapena() { return deskribapena; }
+    public void setDeskribapena(String deskribapena) { this.deskribapena = deskribapena; }
+
     public double getPrezioa() { return prezioa; }
+    public void setPrezioa(double prezioa) { this.prezioa = prezioa; }
+
     public int getStocka() { return stocka; }
+    public void setStocka(int stocka) { this.stocka = stocka; }
+
+    public String getSorkuntzaData() { return sorkuntzaData; }
+    public void setSorkuntzaData(String sorkuntzaData) { this.sorkuntzaData = sorkuntzaData; }
+
     public int getKategoriaId() { return kategoriaId; }
-    public String getIrudiaUrl() { return irudiaUrl; }
+    public void setKategoriaId(int kategoriaId) { this.kategoriaId = kategoriaId; }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | " + izena + " | Data: " + sorkuntzaData + " | Stock: " + stocka + " | Prezioa: " + prezioa + " EUR";
+    }
 }
